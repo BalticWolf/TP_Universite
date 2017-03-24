19386100feea6ed635ed8f829d828c190cbbd3f7
Timothée AUPETIT <timothee.dl21@formation.imie>
2017-03-24 16:07:48 +0100

    Definition des constructeurs de personnes

--- classes/personne.php
+++ classes/personne.php
@@ -30,19 +30,47 @@ abstract class Personne{
 } 
 
 class Etudiant extends Personne{
-	public function __construct($id, $nom, $prenom, $adresse, $age){
-		parent::__construct($id, $nom, $prenom, $adresse, $age)
+	private $_coefFam; // coefficient familial
+	private $_fraisInscr; // frais d'inscription, dépendant du coefficient familial
+	private $_ville; // ville à laquelle est rattaché l'étudiant
+	private $_ufr; // Cursus dans lequel est inscrit l'étudiant
+
+	public function __construct($id, $nom, $prenom, $adresse, $age, $cf, $ville, $ufr){
+		parent::__construct($id, $nom, $prenom, $adresse, $age);
+		$this->_coefFam = $cf;
+		$this->_fraisInscr = calculFrais($cf);
+		$this->_ville = $ville;
+		$this->_ufr = $ufr;
+	}
+	public function calculFrais($cf){
+
+		return $f;
 	}
 }
 
 class Professeur extends Personne{
 	private $_salaire;
-	private $_UFR; // Cursus auquel le professeur est rattaché. 
+	private $_ufr; // Cursus auquel le professeur est rattaché 
+	private $_ville; // ville à laquelle est rattaché le professeur
+	private $_arrCours; // liste de cours qu'enseigne le professeur
+	private $_arrVilles; // liste de villes où enseigne le professeur
 	
-	public function __construct($id, $nom, $prenom, $adresse, $age){
-		parent::__construct($id, $nom, $prenom, $adresse, $age); // appel au constructeur parent
+	public function __construct($id, $nom, $prenom, $adresse, $age, $sal, $ufr, $ville){
+		parent::__construct($id, $nom, $prenom, $adresse, $age);
+		$this->_salaire = $sal;
+		$this->_ufr = $ufr;
+		$this->_ville = $ville;
+		$this->_arrCours = defCours($this->_ufr);
+		$this->_arrVille = defVilles();
+	}
+	
+	private defCours($ufr){
+
 	}
 
+	private defVilles(){
+
+	}
 }
 
 ?>
