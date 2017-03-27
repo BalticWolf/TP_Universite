64a9856b8aaa9735eb01f07a4e5666bd946abf7b
Timothée AUPETIT <timothee.dl21@formation.imie>
2017-03-27 11:57:45 +0200

    miscellaneous display tests

--- index.php
+++ index.php
@@ -3,8 +3,26 @@ require_once('classes/personne.php');
 require_once('classes/universite.php');
 
 function main(){
-	$professeur1 = new Professeur(154);
+/*	$professeur1 = new Professeur(154);
 	print $professeur1;
+	echo "<br/>";*/
+
+	$etudiant1 = new Etudiant(281);
+	print $etudiant1;
+	print "Coef Familial : ".$etudiant1->get('_coefFam')."<br/>";
+	print "Frais inscription : ".$etudiant1->get('_fraisInscr')."<br/>";
+	echo "<br/>";
+/*
+	$univNantes = new Universite('Nantes');
+	$univRennes = new Universite('Rennes');
+	$univNazaire = new Universite('Saint-Nazaire');
+	print $univNantes;
+	print $univRennes;
+	print $univNazaire;
+
+	print $univNantes->get('_bu');
+	print $univNantes->get('_bu')->getLivres('_arrLivresDispo');
+	print $univNantes->get('_bu')->getLivres('_arrLivresEmprunt');*/
 }
 
 main();
