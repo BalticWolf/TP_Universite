16e037b8d31f6e4f5fc9c1d54a971b7836c72380
Timothée AUPETIT <timothee.dl21@formation.imie>
2017-03-27 17:14:40 +0200

    new tests conducted

--- index.php
+++ index.php
@@ -3,24 +3,31 @@ require_once('classes/personne.php');
 require_once('classes/universite.php');
 
 function main(){
-/*	$professeur1 = new Professeur(154);
-	print $professeur1;
-	echo "<br/>";*/
-
-	$etudiant1 = new Etudiant(281);
-	print $etudiant1;
-	print "Coef Familial : ".$etudiant1->get('_coefFam')."<br/>";
-	print "Frais inscription : ".$etudiant1->get('_fraisInscr')."<br/>";
-	echo "<br/>";
-/*
 	$univNantes = new Universite('Nantes');
 	$univRennes = new Universite('Rennes');
 	$univNazaire = new Universite('Saint-Nazaire');
-	print $univNantes;
-	print $univRennes;
-	print $univNazaire;
 
-	print $univNantes->get('_bu');
+	$professeur1 = new Professeur(154);
+	print $professeur1;
+//	print $professeur1->getVilles();
+	echo "<br/>";
+
+/*	$etudiant1 = new Etudiant(281);
+	print $etudiant1;
+	echo "<br/>";*/
+
+/*	print "Coef Familial : ".$etudiant1->get('_coefFam')."<br/>";
+	print "Frais inscription : ".$etudiant1->get('_fraisInscr')."<br/>";
+	echo "<br/>";*/
+
+
+/*	print $univNantes;
+	print $univRennes;
+	print $univNazaire;*/
+//	print $univNantes->getUFR();
+//	print $univNantes->getUFR('SCI');
+
+/*	print $univNantes->get('_bu');
 	print $univNantes->get('_bu')->getLivres('_arrLivresDispo');
 	print $univNantes->get('_bu')->getLivres('_arrLivresEmprunt');*/
 }
